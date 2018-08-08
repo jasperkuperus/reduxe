bc5b3b54d542ddabc019d121d98b12ed94424a35
Jasper Kuperus <bitbucket@jasperkuperus.nl>
2018-08-08 21:57:58 +0200

    Perform an actual dispatch

--- src/reducer.ts
+++ src/reducer.ts
@@ -3,6 +3,15 @@ import createInitialState, { EntityState } from './initialState';
 export default function createReducer<T>() {
   return function reducer(state: EntityState<T> = createInitialState<T>(), action: any) {
     switch (action.type) {
+      case 'ADD_ENTITY':
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [action.entity.id]: action.entity,
+          },
+        };
+
       default:
         return state;
     }
--- test/redux.spec.ts
+++ test/redux.spec.ts
@@ -17,4 +17,26 @@ describe('Reducer', () => {
     expect(state.items).toEqual({});
     expect(state.queries).toEqual({});
   });
+
+  it('should correctly add an entity', () => {
+    const entity = {
+      id: 'id-1',
+      name: 'First Person',
+      age: 30,
+    };
+
+    store.dispatch({
+      type: 'ADD_ENTITY',
+      entity,
+    });
+    const state = store.getState();
+
+    expect(Object.keys(state).length).toEqual(4);
+    expect(state.loaders).toEqual({});
+    expect(state.errors).toEqual({});
+    expect(state.queries).toEqual({});
+
+    expect(Object.keys(state.items).length).toEqual(1);
+    expect(state.items['id-1']).toEqual(entity);
+  });
 });
